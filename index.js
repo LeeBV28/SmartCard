c84f408d674c4aa0f61071dacfa30772db5e42e2
Your Name <you@example.com>
2021-12-03 00:28:17 +0700

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-asd
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 5550;
+app.listen(port, () => {
+    console.log(`Sever is Running http://localhost:${port}`);
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+});
