bb22815ae46415299385041dc22478af2db458f0
Your Name <you@example.com>
2021-12-03 00:35:50 +0700

    index.js fainal

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.listen(port, () => {
     console.log(`Sever is Running http://localhost:${port}`);
 });
 
+app.use(express.static("public"));
+app.set("view engine","ejs");
+
+var bodyParser =  require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true}));
+
+
 app.get('/', function (req, res) {
     res.send('Hello World')
 });
